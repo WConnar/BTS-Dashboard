d2e2ecf4a6a5144bdb9e4f4e72d7e31179cc3ed3
synfy <alexandra.hinton18@ncf.edu>
2021-04-09 19:11:35 -0400

    UI update

---
+++ BTS-Dashboard/src/app/youtube-carousel/youtube-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-youtube-carousel',
+  templateUrl: './youtube-carousel.component.html',
+  styleUrls: ['./youtube-carousel.component.css']
+})
+export class YoutubeCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
