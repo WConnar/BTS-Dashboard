b46f89bc3d1424db1b0970d8551b2b67b02bacd4
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2021-04-09 16:35:09 -0400

    Login Pop Up Window
    Very crude

--- BTS-Dashboard/src/FirebaseConfigs.js
+++ BTS-Dashboard/src/FirebaseConfigs.js
@@ -1,4 +1,22 @@
 const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
+const Googleprovider = new firebase.auth.GoogleAuthProvider();
 const signWithGoogle = document.getElementById('signWithGoogle');
-signWithGoogle.onclick = () => auth.signInWithPopup(provider);
+signWithGoogle.onclick = () => auth.signInWithPopup(Googleprovider);
+const emailSignUp = document.getElementById("emailSignUp");
+const emailSignIn = document.getElementById("emailSignIn");
+emailSignUp.onclick = () => SignUp();
+function SignUp(){
+    let email = document.getElementById('email').value
+    let password = document.getElementById('password').value
+    auth.createUserWithEmailAndPassword(email, password).catch(e=>{
+        console.log(e)
+    })
+}
+auth.onAuthStateChanged(user=>{
+    if(user){
+        console.log(user)
+    }
+    else{
+
+    }
+})
