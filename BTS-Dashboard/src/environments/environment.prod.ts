3bda8aa1c359961436c74e689f84b6c4173cb90e
synfy <alexandra.hinton18@ncf.edu>
2021-04-13 16:25:44 -0400

    Fixed some issues maybe

--- BTS-Dashboard/src/environments/environment.prod.ts
+++ BTS-Dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDrHYlzfwsY3rznx9e6UA6CB4WpqrK1ELY",
+    authDomain: "btsdashboard-d7ad5.firebaseapp.com",
+    projectId: "btsdashboard-d7ad5",
+    storageBucket: "btsdashboard-d7ad5.appspot.com",
+    messagingSenderId: "776148718092",
+    appId: "1:776148718092:web:c7874e5d04f06605139429",
+    measurementId: "G-4R6BDJRM2F"
+  }
 };
