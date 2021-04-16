462d5f8b9b16225d9f7aa07a70c295b8e8443fc0
synfy <alexandra.hinton18@ncf.edu>
2021-04-16 09:54:44 -0400

    Couple updates for class demo

--- BTS-Dashboard/src/app/app.module.ts
+++ BTS-Dashboard/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { YoutubeCarouselComponent } from './youtube-carousel/youtube-carousel.co
     MDBBootstrapModule.forRoot(),
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
