fcfa8a8bd1ee75df7330f8897a3438c119b01d86
synfy <alexandra.hinton18@ncf.edu>
2021-04-21 16:19:04 -0400

    Roughly functional email login/account creation

--- BTS-Dashboard/src/app/nav-bar/nav-bar.component.ts
+++ BTS-Dashboard/src/app/nav-bar/nav-bar.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
 
+export class User {
+  public email!: string;
+  public password!: string;
+ 
+}
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+
+  model = new User();
 
   authState: any = null;
 
