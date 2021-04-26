0dce7683147994a2c90e92c55dbbf12848d17183
synfy <alexandra.hinton18@ncf.edu>
2021-04-26 17:19:28 -0400

    checkboxes on settings page are connected to vars

--- BTS-Dashboard/src/app/settings/settings.component.ts
+++ BTS-Dashboard/src/app/settings/settings.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent{
+
+  trendingTweetsName: string = "Top trending tweets";
+  regionTweetsName: string = "Activity by region";
+  timeTweetsName: string = "Activity by time";
+
+  regionSpotifyName: string = "Top trending tracks by region";
+  timeSpotifyName: string = "Streams over time";
+
+  trendingTweets: boolean = false;
+  regionTweets: boolean = false;
+  timeTweets: boolean = false;
+
+  regionSpotify: boolean = false;
+  timeSpotify: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onChanged(name: string, isChecked: boolean){
+    if(name == this.trendingTweetsName){
+      this.trendingTweets = isChecked;
+    };
+    if(name == this.regionTweetsName){
+      this.regionTweets = isChecked;
+    };
+    if(name == this.timeTweetsName){
+      this.timeTweets = isChecked;
+    };
+    if(name == this.regionSpotifyName){
+      this.regionSpotify = isChecked;
+    };
+    if(name == this.timeSpotifyName){
+      this.timeSpotify = isChecked;
+    };
+  }
+
+  saveThis(){
+    
   }
 
 }
